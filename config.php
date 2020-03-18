3b0a132011d28bc7a8f3b61bd858f7fba3be6219
jonathan.kahre <kahre.jonathan@gmail.com>
2020-03-18 10:40:57 +0100

    Update to respect environment settings if available

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
 function wasa_config($key = '')
 {
+    $wasa_uri = getenv('WASA_URI');
+    $wasa_auth_uri = getenv('WASA_AUTH_URI');
+
     $wasa_configuration = [
-        'base_url' => 'https://b2b.services.wasakredit.se',
-        'access_token_url' => 'https://b2b.services.wasakredit.se/auth/connect/token',
+        'base_url' => $wasa_uri == false ? 'https://b2b.services.wasakredit.se' : $wasa_uri,
+        'access_token_url' => $wasa_auth_uri == false ? 'https://b2b.services.wasakredit.se/auth/connect/token' : $wasa_uri,
         'version' => 'php-2.5',
         'plugin' => null
     ];
