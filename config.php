a77762e0fbe1750403903e037da227ef74731f70
jonathan.kahre <kahre.jonathan@gmail.com>
2020-03-18 11:36:12 +0100

    Fix auth using wrong variable

--- config.php
+++ config.php
@@ -1,15 +1,18 @@
 <?php
 function wasa_config($key = '')
 {
-    $wasa_uri = getenv('WASA_URI');
-    $wasa_auth_uri = getenv('WASA_AUTH_URI');
-
     $wasa_configuration = [
-        'base_url' => $wasa_uri == false ? 'https://b2b.services.wasakredit.se' : $wasa_uri,
-        'access_token_url' => $wasa_auth_uri == false ? 'https://b2b.services.wasakredit.se/auth/connect/token' : $wasa_uri,
+        'base_url' => getEnvOrDefault('WASA_URI', 'https://b2b.services.wasakredit.se'),
+        'access_token_url' => getEnvOrDefault('WASA_AUTH_URI', 'https://b2b.services.wasakredit.se/auth/connect/token'),
         'version' => 'php-2.5',
         'plugin' => null
     ];
 
     return isset($wasa_configuration[$key]) ? $wasa_configuration[$key] : null;
+}
+
+function getEnvOrDefault($key, $default = null)
+{
+    $var = getenv($key);
+    return $var == false ? $default : $var;
 }
