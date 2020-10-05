4d18654eecdd37377cffdffb51a8c128a8a3019e
Gunnar Franklin <frankfurillo@hotmail.com>
2020-10-05 12:48:51 +0200

    #5667 fixed api call to payment methods

--- sdk/Client.php
+++ sdk/Client.php
@@ -146,6 +146,12 @@ class Client
 
     public function get_payment_methods($amount) // @codingStandardsIgnoreLine
     {
-        return $this->api_client->execute($this->base_url . "/v4/payment-options?total_amount=" . $amount , "GET", null);
+        return $this->api_client->execute($this->base_url . "/v4/payment-methods?total_amount=" . $amount , "GET", null);
     }
+
+    public function get_leasing_payment_options($amount) // @codingStandardsIgnoreLine
+    {
+        return $this->api_client->execute($this->base_url . "/v4/leasing/payment-options?total_amount=" . $amount , "GET", null);
+    }
+
 }
